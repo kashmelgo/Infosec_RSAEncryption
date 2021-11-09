23a8e82e6488eebbdf678ee7b11c0ad8206cde52
kashmelgo <55750939+kashmelgo@users.noreply.github.com>
2021-11-09 18:47:31 +0800

    RSA Encryption
    RSA Encryption implemented through Python

---
+++ rsaencryption.py
@@ -0,0 +1,58 @@
+#Kashmel Galil Go       BSCS
+
+def rsaEncryptionAndDecryption(p,q,string):
+    n = p * q
+    z = (p-1) * (q-1)
+    
+    #e, less than n, which has no common factor (other than 1) with z
+    for e in range(1,n):
+        #finding GCD
+        if( z % e != 0):
+            break
+            
+    
+
+    #Find number d, such that ed-1 is exactly divisible z
+    for d in range(1,1000):
+        if(d != z and (e*d-1) != z):
+            if((e*d-1) % z == 0):
+                break
+    print(n)
+    print(z)
+    print(e)
+    print(d)
+
+    #public key (n, e)
+    #private key (n, d)
+
+
+    #encryption (m^e mod n) M IS ORIGINAL TEXT
+    encryptedString = ''
+
+    for i in range(len(string)):
+        s = ord(string[i].upper())
+        encryptedChar = chr((s**e % n))
+        encryptedString += encryptedChar
+
+    print(encryptedString)
+
+
+    #decryption (c^d mod n) C IS ENCRYPTED/CIPHER
+    decryptedString = ''
+    for i in range(len(encryptedString)):
+        s = ord(encryptedString[i])
+        decryptedChar = chr((s**d % n)) 
+        decryptedString += decryptedChar
+
+    print(decryptedString)
+
+    #decryptedString == originalText
+    #FIRST TIME PYTHON BTW EZ XD jk
+    
+    
+
+p = 13
+q = 7
+string = 'ENCRYPTION'
+rsaEncryptionAndDecryption(p,q,string)
+
